eac6790395e52cda2b33fcb2b9fc923874ebafe5
Tamuna Vepkhvadze <tamunvepkhvadze.3@gmail.com>
2025-05-27 17:42:57 +0400

    Add Pages Header

--- src/Admin/dashboard.jsx
+++ src/Admin/dashboard.jsx
@@ -42,6 +42,48 @@ export const Dashboard = () => {
                             
                         </div>
 
+
+
+
+
+
+
+
+<div className={css.tableWrapper}>
+  <div className={css.tableHeader}>
+    <p className={css.cell}>ID</p>
+    <p className={css.cell}>Name</p>
+    <p className={css.cell}>Origin</p>
+    <p className={css.cell}>Caffeine</p>
+    <p className={css.cell}>Price</p>
+    <p className={css.cell}>Actions</p>
+  </div>
+
+  <div className={css.tableRow}>
+    <p className={css.cell}>cof_sample1</p>
+    <p className={css.cell}>Ethiopian Yirgacheffe</p>
+    <p className={css.cell}>Ethiopia</p>
+    <p className={css.cell}>120mg</p>
+    <p className={css.cell}>$4.99</p>
+    <div className={css.cell}>
+      <button className={`${css.btn} ${css.view}`}>View</button>
+      <button className={`${css.btn} ${css.edit}`}>Edit</button>
+      <button className={`${css.btn} ${css.delete}`}>Delete</button>
+    </div>
+  </div>
+</div>
+
+
+
+ 
+  
+
+
+
+
+
+
+
                 <div className={css.coffeeCards}>
 
                     {coffee.map((ingr) => (
--- src/Admin/ingredentsForm.jsx
+++ src/Admin/ingredentsForm.jsx
@@ -3,8 +3,33 @@ import css from "../style.module.css"
 
 
 export const IngredientsForm = ({value, onChange, onSubmit}) => {
+    
 return(
         
+  <div>
+            <div className={css.tableWrapper}>
+                <div className={css.tableHeader}>
+                    <p className={css.cell}>ID</p>
+                    <p className={css.cell}>Name</p>
+                    <p className={css.cell}>Origin</p>
+                    <p className={css.cell}>Caffeine</p>
+                    <p className={css.cell}>Price</p>
+                    <p className={css.cell}>Actions</p>
+                </div>
+
+                <div className={css.tableRow}>
+                    <p className={css.cell}>cof_sample1</p>
+                    <p className={css.cell}>Ethiopian Yirgacheffe</p>
+                    <p className={css.cell}>Ethiopia</p>
+                    <p className={css.cell}>120mg</p>
+                    <p className={css.cell}>$4.99</p>
+                <div className={css.cell}>
+                    <button className={`${css.btn} ${css.view}`}>View</button>
+                    <button className={`${css.btn} ${css.edit}`}>Edit</button>
+                    <button className={`${css.btn} ${css.delete}`}>Delete</button>
+                </div>
+                </div>
+            </div>
            <form className={css.formsecton} onSubmit={onSubmit}>
                 <div className={css.formSection1}>
                 <label>Ingredient Name
@@ -42,6 +67,8 @@ return(
                 
             <button className={`${css.buttonstyle } ${ css.collorbrown} ${ css.manage}`}>Manage Ingredients</button>
            </form>
+
+        </div>   
     
 )
 
